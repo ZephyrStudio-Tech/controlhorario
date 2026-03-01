5a81a9c1ec03ee7314db7a5d0867976d188d2997
ZephyrStudio-Tech <zephyr@zephyrstudio.es>
2026-03-01 08:19:51 +0000

    hecho

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 set -e
 
-# Ejecutar migraciones de base de datos
 echo "Ejecutando migraciones de base de datos..."
 alembic upgrade head
 
-# Cambiar permisos de la carpeta de almacenamiento por si acaso (soluciona el Problema 3)
-chown -R app:app /storage/documents
-
-# Arrancar la aplicación
 echo "Iniciando Gunicorn..."
 exec "$@"
